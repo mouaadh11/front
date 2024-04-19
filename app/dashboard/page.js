d10c1709f97733094a07a3cde3c7e976db1bb410
messabih <msbih.khalil@gmail.com>
2024-04-19 02:06:21 +0100

    refactor(page.js): change function property to clickHandler for clarity in buttons array

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,42 +1,47 @@
-"use client";
-import { useSession } from "next-auth/react";
-import Cookies from "js-cookie";
-import Header from "@/components/header";
+'use client';
+import { useSession } from 'next-auth/react';
+import Cookies from 'js-cookie';
+import Header from '@/components/header';
+import { useRouter } from 'next/navigation';
 
 export default function Dashboard() {
-  const { status } = useSession();
-  console.log(status);
+    const { status } = useSession();
 
-  setTimeout(() => {
-    const token = Cookies.get("session");
-    if (token) {
-      console.log("from dashboard", token);
-    } else {
-      console.log("there is no token");
-    }
-  }, 100);
+    const router = useRouter();
 
-  const buttons = [
-    {
-      buttonTitle: "Add New Device",
-      function: () => {
-        router.push("/device/information");
-      },
-    },
-    {
-      buttonTitle: "Register New Patient",
-      function: () => {
-        router.push("/Patient/add");
-      },
-    },
-  ];
+    setTimeout(() => {
+        const token = Cookies.get('session');
+        if (token) {
+            console.log('from dashboard', token);
+        } else {
+            console.log('there is no token');
+        }
+    }, 100);
 
-  return (
-    <>
-      <Header title="Dashboard" buttons={buttons} />
-      <div>
-        <h1 >APP Name</h1>
-      </div>
-    </>
-  );
+    const buttons = [
+        {
+            buttonTitle: 'Add New Device',
+            clickHandler: () => {
+                router.push('/device/information');
+            },
+        },
+        {
+            buttonTitle: 'Register New Patient',
+            clickHandler: () => {
+                router.push('/Patient/add');
+            },
+        },
+    ];
+
+    return (
+        <>
+            <Header
+                title='Dashboard'
+                buttons={buttons}
+            />
+            <div>
+                <h1>APP Name</h1>
+            </div>
+        </>
+    );
 }
